6b21d4163899d62fd6e5b81a0d4ef898f65d41f4
jvnkoo <123591743+jvnkoo@users.noreply.github.com>
2026-02-04 19:51:07 +0200

    feat(ast): define initial entity and event structures

--- Morphyn.Parser/AST.cs
+++ Morphyn.Parser/AST.cs
@@ -1,6 +1,20 @@
-namespace Morphyn.Runtime.Parser;
-
-public class AST
+namespace Morphyn.Parser
 {
+    /// AST.cs
+    /// Defines data structures for Morphyn
+    /// After parsing, Parser creates objects of these classes
     
+    public class Entity
+    {
+        public string Name; // Entity name, for example "Player"
+        public List<(string Name, int Value)> Has = new(); // Entity properties
+        public List<Event> Events = new(); // Events (on)
+    }
+    
+    // Event (on)
+    public class Event
+    {
+        public string Name; // Event name, for example "damage"
+        public List<string> Statements = new(); // List of properties as a text           
+    }
 }
