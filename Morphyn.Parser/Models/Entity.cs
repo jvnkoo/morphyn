02e587f6e60a3adf148d143d4d85a288d23a4947
jvnkoo <123591743+jvnkoo@users.noreply.github.com>
2026-02-08 22:30:53 +0200

    feat(runtime): implement entity prototyping via deep cloning in pool.add

--- Morphyn.Parser/Models/Entity.cs
+++ Morphyn.Parser/Models/Entity.cs
@@ -8,11 +8,33 @@ namespace Morphyn.Parser
         public required string Name { get; set; }
         public Dictionary<string, object> Fields { get; set; } = new Dictionary<string, object>();
         public List<Event> Events { get; set; } = new List<Event>();
+        public bool IsDestroyed { get; set; } = false;
         public Dictionary<string, Event> EventCache { get; set; } = new();
         
         public void BuildCache() 
         {
             EventCache = Events.ToDictionary(e => e.Name, e => e);
+        }
+        
+        public Entity Clone() 
+        {
+            var clone = new Entity 
+            {
+                Name = this.Name,
+                IsDestroyed = false,
+                Events = this.Events, 
+                EventCache = this.EventCache,
+                Fields = new Dictionary<string, object>()
+            };
+
+            foreach (var kvp in this.Fields) 
+            {
+                if (kvp.Value is MorphynPool pool)
+                    clone.Fields[kvp.Key] = new MorphynPool { Values = new List<object>(pool.Values) };
+                else
+                    clone.Fields[kvp.Key] = kvp.Value;
+            }
+            return clone;
         }
     }
 }
--- Morphyn.Runtime/MorphynRuntime.cs
+++ Morphyn.Runtime/MorphynRuntime.cs
@@ -13,7 +13,7 @@ namespace Morphyn.Runtime
 
         public static void Send(Entity target, string eventName, List<object>? args = null)
         {
-            if (_eventQueue.Any(e => e.EventName == eventName && e.Target == target)) 
+            if (_eventQueue.Any(e => e.EventName == eventName && e.Target == target))
                 return;
             _eventQueue.Enqueue(new PendingEvent(target, eventName, args ?? EmptyArgs));
         }
@@ -57,7 +57,7 @@ namespace Morphyn.Runtime
             {
                 case SetAction set:
                     object value = EvaluateExpression(entity, set.Expression, localScope, data);
-                    
+
                     if (entity.Fields.ContainsKey(set.TargetField))
                         entity.Fields[set.TargetField] = value;
                     else
@@ -69,31 +69,34 @@ namespace Morphyn.Runtime
                 {
                     object result = EvaluateExpression(entity, check.Condition, localScope, data);
                     bool passed = result is bool b && b;
-                    
+
                     if (passed)
                     {
                         if (check.InlineAction != null)
                         {
                             return ExecuteAction(data, entity, check.InlineAction, localScope);
                         }
-                        return true; 
+
+                        return true;
                     }
                     else
                     {
                         if (check.InlineAction != null)
                         {
-                            return true; 
+                            return true;
                         }
+
                         return false;
                     }
                 }
-                
+
                 case SetIndexAction setIdx:
                 {
                     object newValue = EvaluateExpression(entity, setIdx.ValueExpr, localScope, data);
                     int index = Convert.ToInt32(EvaluateExpression(entity, setIdx.IndexExpr, localScope, data)) - 1;
 
-                    if (entity.Fields.TryGetValue(setIdx.TargetPoolName, out var fieldObj) && fieldObj is MorphynPool pool)
+                    if (entity.Fields.TryGetValue(setIdx.TargetPoolName, out var fieldObj) &&
+                        fieldObj is MorphynPool pool)
                     {
                         if (index >= 0 && index < pool.Values.Count)
                         {
@@ -108,15 +111,17 @@ namespace Morphyn.Runtime
                     {
                         throw new Exception($"Target '{setIdx.TargetPoolName}' is not a pool or not found.");
                     }
+
                     return true;
                 }
-                
+
                 case BlockAction block:
                     foreach (var subAction in block.Actions)
                     {
                         if (!ExecuteAction(data, entity, subAction, localScope))
                             return false;
                     }
+
                     return true;
 
                 case EmitAction emit:
@@ -127,15 +132,15 @@ namespace Morphyn.Runtime
 
                     if (emit.EventName == "log")
                     {
-                        var logParts = resolvedArgs.Select(arg => arg is MorphynPool p 
-                            ? "pool[" + string.Join(", ", p.Values) + "]" 
+                        var logParts = resolvedArgs.Select(arg => arg is MorphynPool p
+                            ? "pool[" + string.Join(", ", p.Values) + "]"
                             : arg?.ToString() ?? "null");
                         Console.WriteLine(string.Join(" ", logParts));
                         return true;
                     }
 
                     string targetName = emit.TargetEntityName;
-                    string poolName = emit.EventName; 
+                    string poolName = emit.EventName;
 
                     if (!string.IsNullOrEmpty(targetName) && targetName.Contains('.'))
                     {
@@ -146,13 +151,14 @@ namespace Morphyn.Runtime
                         if (data.Entities.TryGetValue(entityName, out var extEntity) &&
                             extEntity.Fields.TryGetValue(targetPoolName, out var pObj) && pObj is MorphynPool extPool)
                         {
-                            if (HandlePoolCommand(extPool, emit.EventName, resolvedArgs)) return true;
+                            if (HandlePoolCommand(extPool, emit.EventName, resolvedArgs, data)) return true;
                         }
                     }
 
                     if (!string.IsNullOrEmpty(emit.TargetEntityName))
                     {
-                        if (entity.Fields.TryGetValue(emit.TargetEntityName, out var poolObj) && poolObj is MorphynPool localPool)
+                        if (entity.Fields.TryGetValue(emit.TargetEntityName, out var poolObj) &&
+                            poolObj is MorphynPool localPool)
                         {
                             if (emit.EventName == "each")
                             {
@@ -161,23 +167,31 @@ namespace Morphyn.Runtime
                                 foreach (var item in localPool.Values)
                                 {
                                     if (item is Entity subE) Send(subE, subEventName, subArgs);
-                                    else if (item is string eName && data.Entities.TryGetValue(eName, out var extE)) Send(extE, subEventName, subArgs);
+                                    else if (item is string eName && data.Entities.TryGetValue(eName, out var extE))
+                                        Send(extE, subEventName, subArgs);
                                 }
+
                                 return true;
                             }
 
-                            if (HandlePoolCommand(localPool, emit.EventName, resolvedArgs)) return true;
+                            if (HandlePoolCommand(localPool, emit.EventName, resolvedArgs, data)) return true;
                         }
                     }
 
-                    Entity? target = string.IsNullOrEmpty(targetName) ? entity : 
-                        (data.Entities.TryGetValue(targetName, out var e) ? e : null);
+                    Entity? target = string.IsNullOrEmpty(targetName)
+                        ? entity
+                        : (data.Entities.TryGetValue(targetName, out var e) ? e : null);
 
-                    if (target != null) {
+                    if (target != null)
+                    {
                         Send(target, emit.EventName, resolvedArgs);
-                    } else {
-                        Console.WriteLine($"[ERROR] Target entity '{targetName}' not found for event '{emit.EventName}'");
                     }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"[ERROR] Target entity '{targetName}' not found for event '{emit.EventName}'");
+                    }
+
                     return true;
                 }
 
@@ -185,26 +199,36 @@ namespace Morphyn.Runtime
                     return true;
             }
         }
-        
-        private static bool HandlePoolCommand(MorphynPool pool, string command, List<object> args)
+
+        private static bool HandlePoolCommand(MorphynPool pool, string command, List<object> args, EntityData data)
         {
             switch (command)
             {
                 case "add":
-                    pool.Values.Add(args[0]);
+                    string typeName = args[0].ToString()!;
+                    if (data.Entities.TryGetValue(typeName, out var prototype))
+                    {
+                        var newEntity = prototype.Clone();
+                        pool.Values.Add(newEntity);
+                        Send(newEntity, "init");
+                    }
+                    else
+                    {
+                        pool.Values.Add(args[0]);
+                    }
                     return true;
-                case "push": 
+                case "push":
                     pool.Values.Insert(0, args[0]);
                     return true;
-                case "insert": 
+                case "insert":
                     pool.Values.Insert(Convert.ToInt32(args[0]) - 1, args[1]);
                     return true;
-                case "remove_at": 
+                case "remove_at":
                     int idxRem = Convert.ToInt32(args[0]) - 1;
                     if (idxRem >= 0 && idxRem < pool.Values.Count)
                         pool.Values.RemoveAt(idxRem);
                     return true;
-                case "remove": 
+                case "remove":
                     pool.Values.Remove(args[0]);
                     return true;
                 case "pop":
@@ -213,7 +237,7 @@ namespace Morphyn.Runtime
                 case "shift":
                     if (pool.Values.Count > 0) pool.Values.RemoveAt(0);
                     return true;
-                case "swap": 
+                case "swap":
                     int i1 = Convert.ToInt32(args[0]) - 1;
                     int i2 = Convert.ToInt32(args[1]) - 1;
                     if (i1 >= 0 && i1 < pool.Values.Count && i2 >= 0 && i2 < pool.Values.Count)
@@ -222,12 +246,13 @@ namespace Morphyn.Runtime
                         pool.Values[i1] = pool.Values[i2];
                         pool.Values[i2] = temp;
                     }
+
                     return true;
-                case "clear": 
+                case "clear":
                     pool.Values.Clear();
                     return true;
                 default:
-                    return false; 
+                    return false;
             }
         }
     }
